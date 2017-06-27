0693cf7e0591e71162a3adfcd5fef50393387b25
Mike <mgrahamjo@gmail.com>
2017-06-26 20:45:25 -0700

    move admin app to pilothound repo

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-
-    res.render('index', {
-        path: req.path
-    });
-
-};
--- util/swap.js
+++
@@ -1,5 +0,0 @@
-module.exports = (key, functions) => {
-    
-    return functions[key] ? functions[key]() : functions.default ? functions.default() : undefined; // eslint-disable-line
-    
-};
